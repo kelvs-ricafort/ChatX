f1e6144f61f03c2263d45a37283c83afc3d527a6
Kelvin Ricafort <kelvs.r@outlook.com>
2024-11-05 15:54:52 +0800

    Initial Commit

---
+++ ChatX/ChatXApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatXApp.swift
+//  ChatX
+//
+//  Created by Kelvin on 11/5/24.
+//
+
+import SwiftUI
+
+@main
+struct ChatXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
